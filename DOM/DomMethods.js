0d13939b48d272153927f51142e76fcda9fea2f9
ANKITA PRASAD <ankitaprasad2799@gmail.com>
2024-11-20 15:28:08 +0530

    Add files via upload

---
+++ DOM/DomMethods.js
@@ -0,0 +1,31 @@
+//HOW TO TARGET DOM OBJECT:
+// 1.ID
+// 2.CLASS NAME
+// 3.TAG NAME
+//4.DOCUMENT
+//5.DOCUMENT.ALL 
+//6.DOCUMENT.HEAD
+//7.DOCUMENT.TITLE
+//8.DOCUMENT.BODY
+//9.DOCUMENT.LINKS
+//10.DOCUMENT.IMAGES
+//11.DOCUMENT.FORMS
+//12.DOCUMENT.DOCTYPE
+//13.DOCUMENT.URL
+//14.DOCUMENT.DOMAIN
+//15.DOCUMENT.BASEURL
+
+
+// var element;
+// element = document.getElementById("head");
+// document.write(element);
+
+// element = document.getElementsByClassName("welcome");
+
+// element = document.getElementsByTagName("h5");
+
+
+
+
+
+
---
+++ DOM/GetWithDom.js
@@ -0,0 +1,20 @@
+//WHAT WE CAN GET WITH DOM
+//  1.HTML
+//  2.TEXT
+//  3.ATTRIBUTE
+
+//DOM GET METHODS
+    // ->innerHTML
+    // ->innerTEXT
+    // ->getAttribute
+    // ->getAttributeNode
+    // ->Attributes
+
+
+var elements;
+elements=document.getElementById('header').innerHTML;
+console.log(elements);
+
+// var elements;
+// elements=document.getElementsByTagName('header').innerHTML;
+// console.log(elements);
